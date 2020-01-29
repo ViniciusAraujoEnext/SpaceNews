0f5105afee17ecdc096b0033a3fada4102f9638c
Vinicius Mendes de Araujo <vinicius.araujo@enext.local>
2020-01-29 12:05:16 -0300

    banner finalizado com timer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$('.banner').slick();
+$('.banner').slick({
+	dots: true,
+	infinite: true,
+	speed: 300,
+	adptiveHeight: true,
+	slidesToShow: 1,
+	autoplay: true,
+	autoplaySpeed: 1000
+});
--- main.js
+++
@@ -1,6 +0,0 @@
-$('#content').slick({
-	dots: true,
-	infinite: true.
-	speed 3000,
-	slidesToShow: 1
-)};
