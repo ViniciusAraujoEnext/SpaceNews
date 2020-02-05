140c168a98b99f5ad0921627c5f32abdef0f69d7
Vinicius Mendes de Araujo <vinicius.araujo@enext.local>
2020-02-05 12:01:40 -0300

    slider primeira pagina arrumado

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $('.section-primary-slider').slick({
-infinite: true,
+// infinite: true,
 speed: 300,
 autoplay: true,
 autoplaySpeed: 3500,
 });
 
 $('.banner-slider').slick({
-infinite: true,
+// infinite: true,
 speed: 300,
 autoplay: true,
 autoplaySpeed: 3500,
